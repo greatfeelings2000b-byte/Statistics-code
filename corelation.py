94f47829f2401a3950eb4fbcf16937f3d7e2fcb0
Huzaifa Khan <greatfeelings2000b@gmail.com>
2026-03-18 13:47:38 -0700

    this is the simple code of telling the correlation of two datasets

--- corelation.py
+++ corelation.py
@@ -1,4 +1,19 @@
-corr(x,x) = 1.0  
+import numpy as np
+
+x = np.array([1,2,3,4,5])
+y = np.array([2,4,6,8,10])
+
+correlation = np.corrcoef(x, y)
+
+print(correlation)
+
+# [[corr(x,x), corr(x,y)],
+#  [corr(y,x), corr(y,y)]]
+
+# [[1. 1.]
+#  [1. 1.]]
+
+# corr(x,x) = 1.0  
 # Any variable compared with itself has perfect correlation.
 
 # corr(x,y) = 1.0  
